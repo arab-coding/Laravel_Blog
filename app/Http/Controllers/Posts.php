772c43e24c84c5b54bcb70ad9a80a7ceba998710
ali <ali@alis-MacBook-Pro.local>
2019-02-12 11:13:09 +0200

    Initial commit

---
+++ app/Http/Controllers/Posts.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Posts extends Controller
+{
+
+    public function post($id1,$name){
+
+//        return view('main.post')->with('id_post',$id);
+        return view('main.post',compact('id1','name'));
+    }
+
+
+
+
+
+
+
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index($id)
+    {
+        //
+        return "welcome index" . $id;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return "create";
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        return "show" . $id ;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+    Route::get('/', function () {
+        return view('welcome');
+    });
+//
+//    Route::get('/book',function(){
+//        return "welcome";
+//    });
+//
+//    Route::get('/post/{id}',function($id){
+//        return "post num " . $id;
+//    });
+    // Route::get($uri, $callback);
+    // Route::post($uri, $callback);
+    // Route::put($uri, $callback);
+    // Route::patch($uri, $callback);
+    // Route::delete($uri, $callback);
+    // Route::options($uri, $callback);
+
+//    Route::get('/post','Posts@index');
+//
+//    Route::get('/post/{id}','Posts@index');
+
+
+//    Route::resource('post','Posts');
+    Route::get('post/{id}/{name}','posts@post');
